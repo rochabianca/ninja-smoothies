fbcf74febc35befe055b0cf6688d7f6484340e2d
Bianca <bianca.rocha1711@gmail.com>
2019-01-23 09:00:56 -0300

    deleting a smoothie on firebase

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -26,9 +26,15 @@ export default {
   },
   methods: {
     deleteSmoothie(id) {
-      this.smoothies = this.smoothies.filter(smoothie => {
-        return smoothie.id !== id;
-      });
+      // Delete doc from firestore
+      db.collection("smoothies")
+        .doc(id)
+        .delete()
+        .then(() => {
+          this.smoothies = this.smoothies.filter(smoothie => {
+            return smoothie.id !== id;
+          });
+        });
     }
   },
   created() {
