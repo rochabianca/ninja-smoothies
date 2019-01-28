5a28a9dd944383172675ff85455a63ab8ac79657
Bianca <bianca.rocha1711@gmail.com>
2019-01-28 09:08:20 -0300

    edit button

---
+++ src/components/EditSmoothie.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="edit-smoothie container">
+    <h2>Edit smoothie {{this.$route.params.smoothie_slug}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EditSmoothie",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+</style>
