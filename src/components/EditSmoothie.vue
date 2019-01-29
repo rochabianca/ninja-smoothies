79dd57ac8a3660988919348d885c2ae6f617950f
Bianca <bianca.rocha1711@gmail.com>
2019-01-29 09:03:02 -0300

    edit form and methods

--- src/components/EditSmoothie.vue
+++ src/components/EditSmoothie.vue
@@ -1,6 +1,33 @@
 <template>
   <div v-if="smoothie" class="edit-smoothie container">
     <h2>Edit {{smoothie.title}} Smoothie</h2>
+    <form @submit.prevent="EditSmoothie">
+      <div class="field title">
+        <label for="title">Smoothie Title</label>
+        <input type="text" name="title" v-model="smoothie.title">
+      </div>
+
+      <div v-for="(ingredient, index) in smoothie.ingredients" :key="index" class="field">
+        <label for="ingredient">Ingredient:</label>
+        <input type="text" name="ingredient" v-model="smoothie.ingredients[index]">
+        <i class="material-icons delete" @click="deleteIngredient(ingredient)">delete</i>
+      </div>
+
+      <div class="field add-ingredient">
+        <label for="add-ingredient">Add an Ingredient</label>
+        <input
+          type="text"
+          name="add-ingredient"
+          @keydown.tab.prevent="addIngredient"
+          v-model="another"
+        >
+      </div>
+
+      <div class="field center-align">
+        <p v-if="feedback" class="red-text">{{feedback}}</p>
+        <button class="btn pink">Update Smoothie</button>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -12,8 +39,29 @@ export default {
   data() {
     return {
       slug: this.$route.params.smoothie_slug,
-      smoothie: null
+      smoothie: null,
+      another: null,
+      feedback: null
     };
+  },
+  methods: {
+    EditSmoothie() {
+      console.log(this.smoothie.title, this.smoothie.ingredients);
+    },
+    addIngredient() {
+      if (this.another) {
+        this.smoothie.ingredients.push(this.another);
+        this.another = null;
+        this.feedback = null;
+      } else {
+        this.feedback = "You must enter a value to add a ingredient";
+      }
+    },
+    deleteIngredient(ingredient) {
+      this.smoothie.ingredients = this.smoothie.ingredients.filter(i => {
+        return i !== ingredient;
+      });
+    }
   },
   created() {
     let ref = db.collection("smoothies").where("slug", "==", this.slug);
@@ -27,5 +75,27 @@ export default {
 };
 </script>
 
-<style>
+<style lang='scss'>
+.edit-smoothie {
+  margin-top: 60px;
+  padding: 20px;
+  max-width: 500px;
+
+  h2 {
+    font-size: 2em;
+    margin: 20px auto;
+  }
+  .field {
+    margin: 20px auto;
+    position: relative;
+  }
+  .delete {
+    position: absolute;
+    right: 0;
+    bottom: 16px;
+    color: #aaa;
+    font-size: 1.4em;
+    cursor: pointer;
+  }
+}
 </style>
