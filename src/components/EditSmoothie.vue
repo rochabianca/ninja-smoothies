1ab06207e9a4099c1d957d7e1f5a48e39911f769
Bianca <bianca.rocha1711@gmail.com>
2019-01-29 08:42:18 -0300

    getting smoothie data on edit form

--- src/components/EditSmoothie.vue
+++ src/components/EditSmoothie.vue
@@ -1,14 +1,28 @@
 <template>
-  <div class="edit-smoothie container">
-    <h2>Edit smoothie {{this.$route.params.smoothie_slug}}</h2>
+  <div v-if="smoothie" class="edit-smoothie container">
+    <h2>Edit {{smoothie.title}} Smoothie</h2>
   </div>
 </template>
 
 <script>
+import db from "@/components/firebase/init";
+
 export default {
   name: "EditSmoothie",
   data() {
-    return {};
+    return {
+      slug: this.$route.params.smoothie_slug,
+      smoothie: null
+    };
+  },
+  created() {
+    let ref = db.collection("smoothies").where("slug", "==", this.slug);
+    ref.get().then(snapshot => {
+      snapshot.forEach(doc => {
+        this.smoothie = doc.data();
+        this.smoothie.id = doc.id;
+      });
+    });
   }
 };
 </script>
